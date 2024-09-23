731684e61898fe58db85bf2b7b4f7d43ad489899
Michiel Keijts <michiel@normit.nl>
2024-09-23 15:09:16 +0200

    Update type compatibility

---
+++ config/Migrations/20231214114611_InitDatabaseMessages.php
@@ -0,0 +1,221 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\AbstractMigration;
+
+class InitDatabaseMessages extends AbstractMigration
+{
+    /**
+     * Up Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-up-method
+     * @return void
+     */
+    public function up()
+    {
+        $this->table('messages')
+            ->addColumn('group_id', 'string', [
+                'default' => null,
+                'limit' => 34,
+                'null' => true,
+            ])
+            ->addColumn('name', 'string', [
+                'limit' => 45,
+                'null' => false,
+            ])
+            ->addColumn('body', 'text', [
+                'default' => null,
+                'limit' => 4294967295,
+                'null' => true,
+            ])
+            ->addColumn('message_read', 'boolean', [
+                'default' => 0,
+                'null' => false,
+            ])
+            ->addColumn('created', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addColumn('created_by', 'integer', [
+                'default' => null,
+                'limit' => 11,
+                'null' => true,
+            ])
+            ->addColumn('modified', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addColumn('modified_by', 'integer', [
+                'default' => null,
+                'limit' => 11,
+                'null' => true,
+            ])
+            ->addIndex(
+                [
+                    'created_by',
+                    'created',
+                    'group_id'
+                ]
+            )
+            ->addIndex(
+                [
+                    'created',
+                    'group_id'
+                ]
+            )
+            ->addIndex(
+                [
+                    'created_by',
+                    'created',
+                    'name',
+                    'group_id',
+                ]
+            )
+            ->addIndex(
+                [
+                    'name',
+                    'group_id',
+                ]
+            )
+            ->addIndex(
+                [
+                    'name'
+                ],
+                [
+                    'type' => 'fulltext'
+                ]
+            )
+            ->create();
+
+        $this->table('notifications')
+            ->addColumn('site_id', 'integer', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+                'signed' => true,
+            ])
+            ->addColumn('language', 'string', [
+                'default' => null,
+                'limit' => 10,
+                'null' => true,
+            ])
+            ->addColumn('name', 'string', [
+                'default' => null,
+                'limit' => 150,
+                'null' => true,
+            ])
+            ->addColumn('body', 'text', [
+                'default' => null,
+                'limit' => 16777215,
+                'null' => true,
+            ])
+            ->addColumn('config', 'text', [
+                'default' => null,
+                'limit' => 16777215,
+                'null' => true,
+            ])
+            ->addColumn('recipients', 'text', [
+                'default' => null,
+                'limit' => 16777215,
+                'null' => true,
+            ])
+            ->addColumn('sent', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addColumn('template', 'string', [
+                'default' => null,
+                'limit' => 150,
+                'null' => true,
+            ])
+            ->addColumn('layout', 'string', [
+                'default' => null,
+                'limit' => 150,
+                'null' => true,
+            ])
+            ->addColumn('created', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addColumn('created_by', 'integer', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+                'signed' => true,
+            ])
+            ->addColumn('modified', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addColumn('modified_by', 'integer', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+                'signed' => true,
+            ])
+            ->addColumn('deleted', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addColumn('deleted_by', 'integer', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+                'signed' => true,
+            ])
+            ->addIndex(
+                [
+                    'site_id',
+                    'language',
+                    'name',
+                ],
+                [
+                    'name' => 'index5',
+                ]
+            )
+            ->addIndex(
+                [
+                    'site_id',
+                    'language',
+                    'deleted',
+                    'sent',
+                ],
+                [
+                    'name' => 'deleted',
+                ]
+            )
+            ->addIndex(
+                [
+                    'site_id',
+                    'language',
+                    'deleted',
+                    'created',
+                    'sent',
+                ],
+                [
+                    'name' => 'deleted_2',
+                ]
+            )
+            ->create();
+    }
+
+    /**
+     * Down Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-down-method
+     * @return void
+     */
+    public function down()
+    {
+        $this->table('messages')->drop()->save();
+        $this->table('notifications')->drop()->save();
+    }
+}
---
+++ config/Migrations/20240920101520_RenameTable.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\AbstractMigration;
+
+class RenameTable extends AbstractMigration
+{
+    /**
+     * Up Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-up-method
+     * @return void
+     */
+    public function change()
+    {
+        $this->table('messages')->rename('cake_notifications_messages')->update();
+        $this->table('notifications')->rename('cake_notifications_notifications')->renameColumn('language','locale')->update();
+    }
+}
